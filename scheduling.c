eff32c6aaac2515a4724c82944b441fa102481b7
Johnny Zhou <jonathanqyz@gmail.com>
2022-10-03 13:37:45 -0400

    updated queue implementation

--- scheduling.c
+++ scheduling.c
@@ -6,66 +6,48 @@
 #include <math.h>
 ////////////////////////////////
 struct Queue {
-  int front, rear, size;
+  int front, back, length;
   unsigned capacity;
-  int *array;
+  int *arr;
 };
 
-// function to create a queue
-// of given capacity.
-// It initializes size of queue as 0
+// create a queue of given capacity.
+// set length of queue as 0
 struct Queue *createQueue(unsigned capacity) {
   struct Queue *queue = (struct Queue *)malloc(sizeof(struct Queue));
   queue->capacity = capacity;
-  queue->front = queue->size = 0;
-
-  // This is important, see the enqueue
-  queue->rear = capacity - 1;
-  queue->array = (int *)malloc(queue->capacity * sizeof(int));
+  queue->front = 0;
+  queue->length = 0;
+  queue->back = capacity - 1;
+  queue->arr = (int *)malloc(queue->capacity * sizeof(int)); //allocate space for the amount of queue elements specified
   return queue;
 }
 
-// Queue is full when size becomes
-// equal to the capacity
-int isFull(struct Queue *queue) { return (queue->size == queue->capacity); }
-
-// Queue is empty when size is 0
-int isEmpty(struct Queue *queue) { return (queue->size == 0); }
-
-// Function to add an item to the queue.
-// It changes rear and size
+// add an item to the queue
 void enqueue(struct Queue *queue, int item) {
-  if (isFull(queue))
-    return;
-  queue->rear = (queue->rear + 1) % queue->capacity;
-  queue->array[queue->rear] = item;
-  queue->size = queue->size + 1;
+  queue->back = (queue->back + 1) % queue->capacity; // in case of overflow
+  queue->arr[queue->back] = item;
+  queue->length = queue->length + 1;
 }
 
-// Function to remove an item from queue.
-// It changes front and size
+// returns front of the queue, decreases queue length by 1
 int dequeue(struct Queue *queue) {
-  if (isEmpty(queue))
+  if (queue->length == 0)
     return INT_MIN;
-  int item = queue->array[queue->front];
-  queue->front = (queue->front + 1) % queue->capacity;
-  queue->size = queue->size - 1;
+  int item = queue->arr[queue->front]; //return the item at the front
+  queue->front = (queue->front + 1) % queue->capacity; // update the front of the queue
+  queue->length = queue->length - 1;
   return item;
 }
 
-// Function to get front of queue
-int front(struct Queue *queue) {
-  if (isEmpty(queue))
-    return INT_MIN;
-  return queue->array[queue->front];
+// returns the num stored at the front of the queue 
+int front(struct Queue* queue)
+{
+  if (queue->length == 0)
+      return INT_MIN;
+  return queue->arr[queue->front];
 }
 
-// Function to get rear of queue
-int rear(struct Queue *queue) {
-  if (isEmpty(queue))
-    return INT_MIN;
-  return queue->array[queue->rear];
-}
 //////////////////////////////////
 void fcfs(FILE *fp, char *filename) { // first come first serve implementation
   int c; // number of processes to schedule
@@ -476,7 +458,6 @@ int main(int argc, char *argv[]) {
 
   // close the processes file
   fclose(fp);
-
   return 0;
 }
 
